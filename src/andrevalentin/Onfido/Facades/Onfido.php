9a0fd86a3062d26d3d46f5d629ae28101df6e34b
André Valentin <andre@worksome.com>
2021-03-03 17:07:19 +0100

    Updating to the latest Onfido PHP API Client, also adding some docblock helpers

--- src/andrevalentin/Onfido/Facades/Onfido.php
+++ src/andrevalentin/Onfido/Facades/Onfido.php
@@ -3,7 +3,19 @@
 namespace andrevalentin\Onfido\Facades;
 
 use Illuminate\Support\Facades\Facade;
+use Onfido\Api\DefaultApi;
+use Onfido\Model\Applicant;
+use Onfido\Model\Check;
 
+/**
+ * Class Onfido
+ * @package andrevalentin\Onfido\Facades
+ *
+ * @method static Applicant createApplicant(array $applicant)
+ * @method static Check createCheck(array $check)
+ *
+ * @see DefaultApi
+ */
 class Onfido extends Facade
 {
     protected static function getFacadeAccessor()
